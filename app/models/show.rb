5ced5756a5482447805f0bd3056f92c78f690629
Sean Langton <slangton1@gmail.com>
2019-01-22 00:29:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,15 +5,15 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.find_by({rating: self.highest_rating})
+    self.find_by({rating: self.highest_rating})
   end
 
   def self.lowest_rating
-
+    self.minimum(:rating)
   end
 
   def self.least_popular_show
-
+        self.find_by({rating: self.lowest_rating})
   end
 
   def self.ratings_sum
