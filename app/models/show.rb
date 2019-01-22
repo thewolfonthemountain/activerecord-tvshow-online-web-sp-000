38d31b44c51b01bc3007a1bac8897c81d2eb3d90
Sean Langton <slangton1@gmail.com>
2019-01-22 00:25:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,29 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating)
+  end
+
+  def self.lowest_rating
+
+  end
+
+  def self.least_popular_show
+
+  end
+
+  def self.ratings_sum
+
+  end
+
+  def self.popular_shows
+
+  end
+
+  def self.shows_by_alphabetical_order
+
+  end
+
 end
