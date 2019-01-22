7e0410528e0295eb4d79127555baae57f96d88fa
Sean Langton <slangton1@gmail.com>
2019-01-22 00:15:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,6 @@
-class CreateShows < ActiveRecord::Migration[5.2]
+class CreateShows < ActiveRecord::Migration[4.2]
   def change
     create_table :shows do |t|
-    binding.pry
     t.string :name
     end
   end
