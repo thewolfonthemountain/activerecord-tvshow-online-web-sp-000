bf16d8c5cd451ae9f8bfd5f5302f82c7908c36be
Sean Langton <slangton1@gmail.com>
2019-01-22 00:04:50 +0000

    Automatically backed up by Learn

---
+++ db/mirgate/001_create_shows.rb
@@ -0,0 +1,7 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
