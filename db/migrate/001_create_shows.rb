ed8f4ad0c450584642e95761316ed36421add22e
Sean Langton <slangton1@gmail.com>
2019-01-22 00:18:45 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
-class CreateShows < ActiveRecord::Migration[4.2]
+class CreateShows < ActiveRecord::Migration[5.1]
   def change
     create_table :shows do |t|
     t.string :name
     t.string :network
     t.string :day
-    t.integer :rating       
+    t.integer :rating
     end
   end
 end
